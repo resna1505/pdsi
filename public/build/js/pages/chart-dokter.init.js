8f894801f3a1b48954c5705a1c03bc2d4a094542
resna1505 <resnapangestu01@gmail.com>
2025-04-07 19:14:11 +0700

    add chart-dokter.init.js for custom donut chart

---
+++ public/build/js/pages/chart-dokter.init.js
@@ -0,0 +1,255 @@
+/*
+Template Name: PDSI - Admin & Dashboard Template
+Author: ICT PDSI
+Website: https://ICT PDSI.com/
+Contact: ICT PDSI@gmail.com
+File: Widgets init js
+*/
+
+// get colors array from the string
+function getChartColorsArray(chartId) {
+    if (document.getElementById(chartId) !== null) {
+        var colors = document.getElementById(chartId).getAttribute("data-colors");
+        if (colors) {
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    if (color) return color;
+                    else return newValue;;
+                } else {
+                    var val = value.split(',');
+                    if (val.length == 2) {
+                        var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
+                        rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
+                        return rgbaColor;
+                    } else {
+                        return newValue;
+                    }
+                }
+            });
+        } else {
+            console.warn('data-colors Attribute not found on:', chartId);
+        }
+    }
+}
+
+// Total Portfolio Donut Charts
+var donutchartportfolioColors = getChartColorsArray("portfolio_donut_charts");
+if (donutchartportfolioColors) {
+    var options = {
+        series: [320, 250, 130, 5],
+        labels: ["Dr. Umum", "Dr. Gigi", "Dr. Spesialist", "Dr. Sub Spesialist"],
+        chart: {
+            type: "donut",
+            height: 210,
+        },
+
+        plotOptions: {
+            pie: {
+                size: 100,
+                offsetX: 0,
+                offsetY: 0,
+                donut: {
+                    size: "70%",
+                    labels: {
+                        show: true,
+                        name: {
+                            show: true,
+                            fontSize: '18px',
+                            offsetY: -5,
+                        },
+                        value: {
+                            show: true,
+                            fontSize: '20px',
+                            color: '#343a40',
+                            fontWeight: 500,
+                            offsetY: 5,
+                            formatter: function (val) {
+                                return val + " Dokter";
+                            }                            
+                        },
+                        total: {
+                            show: true,
+                            fontSize: '13px',
+                            label: 'Total Doctors',
+                            color: '#9599ad',
+                            fontWeight: 500,
+                            formatter: function (w) {
+                                return w.globals.seriesTotals.reduce(function (a, b) {
+                                    return a + b
+                                }, 0) + " Dokter";
+                            }                            
+                        }
+                    }
+                },
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        legend: {
+            show: false,
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value) {
+                    return "$" + value;
+                }
+            }
+        },
+        stroke: {
+            lineCap: "round",
+            width: 2
+        },
+        colors: donutchartportfolioColors,
+    };
+
+    var chart = new ApexCharts(document.querySelector("#portfolio_donut_charts"), options);
+    chart.render();
+}
+
+var linechartcustomerColors = getChartColorsArray("customer_impression_charts");
+if (linechartcustomerColors) {
+    var options = {
+        series: [{
+            name: "daftar",
+            type: "area",
+            data: [34, 65, 46, 68, 49, 61, 42, 44, 78, 52, 63, 67],
+        },
+        {
+            name: "aktif",
+            type: "bar",
+            data: [
+                89.25, 98.58, 68.74, 108.87, 77.54, 84.03, 51.24, 28.57, 92.57, 42.36,
+                88.51, 36.57,
+            ],
+        },
+        {
+            name: "non aktif",
+            type: "line",
+            data: [8, 12, 7, 17, 21, 11, 5, 9, 7, 29, 12, 35],
+        },
+        ],
+        chart: {
+            height: 310,
+            type: "line",
+            toolbar: {
+                show: false,
+            },
+        },
+        stroke: {
+            curve: "straight",
+            dashArray: [0, 0, 8],
+            width: [0.1, 0, 2],
+        },
+        fill: {
+            opacity: [0.03, 0.9, 1],
+        },
+        markers: {
+            size: [0, 0, 0],
+            strokeWidth: 2,
+            hover: {
+                size: 4,
+            },
+        },
+        xaxis: {
+            categories: [
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec",
+            ],
+            axisTicks: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+        },
+        grid: {
+            show: true,
+            xaxis: {
+                lines: {
+                    show: true,
+                },
+            },
+            yaxis: {
+                lines: {
+                    show: false,
+                },
+            },
+            padding: {
+                top: 0,
+                right: -2,
+                bottom: 15,
+                left: 10,
+            },
+        },
+        legend: {
+            show: true,
+            horizontalAlign: "center",
+            offsetX: 0,
+            offsetY: -5,
+            markers: {
+                width: 9,
+                height: 9,
+                radius: 6,
+            },
+            itemMargin: {
+                horizontal: 10,
+                vertical: 0,
+            },
+        },
+        plotOptions: {
+            bar: {
+                columnWidth: "20%",
+                barHeight: "100%",
+                borderRadius: [8],
+            },
+        },
+        colors: linechartcustomerColors,
+        tooltip: {
+            shared: true,
+            y: [{
+                formatter: function (y) {
+                    if (typeof y !== "undefined") {
+                        return y.toFixed(0);
+                    }
+                    return y;
+                },
+            },
+            {
+                formatter: function (y) {
+                    if (typeof y !== "undefined") {
+                        return "$" + y.toFixed(2) + "k";
+                    }
+                    return y;
+                },
+            },
+            {
+                formatter: function (y) {
+                    if (typeof y !== "undefined") {
+                        return y.toFixed(0) + " Sales";
+                    }
+                    return y;
+                },
+            },
+            ],
+        },
+    };
+    var chart = new ApexCharts(
+        document.querySelector("#customer_impression_charts"),
+        options
+    );
+    chart.render();
+}
