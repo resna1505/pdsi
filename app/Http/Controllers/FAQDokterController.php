db4db312cfb9430c7839a1814111c2ae1edb785b
resna1505 <resnapangestu01@gmail.com>
2025-05-31 04:30:36 +0700

    -

--- app/Http/Controllers/FAQDokterController.php
+++ app/Http/Controllers/FAQDokterController.php
@@ -13,6 +13,6 @@ class FAQDokterController extends Controller
         $articles = FAQItem::orderBy('created_at', 'desc')->get();
         $categories = FAQCategory::with('items')->get();
 
-        return view('admin.faq', compact('articles', 'categories'));
+        return view('member.faq', compact('articles', 'categories'));
     }
 }
