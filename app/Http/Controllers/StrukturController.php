79d1e03d323974266b816d659c17621e770ad05a
resna1505 <resnapangestu01@gmail.com>
2025-07-21 21:50:26 +0700

    -

--- app/Http/Controllers/StrukturController.php
+++ app/Http/Controllers/StrukturController.php
@@ -21,7 +21,7 @@ class StrukturController extends Controller
             $request->validate([
                 'title' => 'required|string|max:255',
                 'subtitle' => 'required|string|max:500',
-                'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif',
             ]);
 
             $imagePath = null;
